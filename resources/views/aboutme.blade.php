f4f707af832bb4d34e7ea4688b2504a356651452
AntonioSusca <antonio.esse91@gmail.com>
2024-08-19 20:59:51 +0200

    inizio

---
+++ resources/views/aboutme.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+<body>
+    <h1>Ciao</h1>
+</body>
+</html>
+
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<x-layout>
+
+
+
+<body class="bg-slate-200">
+    <h1 class="underline text-xl m-3 justify-center text-center "> Hello World </h1>
+    <div class="flex">
+        <div class="flex-1 rounded-xl bg-slate-500 p-10 m-2 ">01</div>
+        <div class="flex-1 rounded-xl bg-slate-500 p-10 m-2 ">02</div>
+    </div>
+
+
+    <div class="rounded-xl bg-slate-500 p-2 m-4  ">
+        <div class=" flex-1 red bg-red-600 ">
+        </div>
+        <div class=" flex-1 bg-slate-700 ">
+        </div>
+
+        <p class="text-center"> Ciao mi chiamo Antonio Susca</p>
+
+    </div>
+    <h1 class="text-center text-black"><a href="/aboutme">ABOUT ME</a></h1>
+
+</x-layout>
