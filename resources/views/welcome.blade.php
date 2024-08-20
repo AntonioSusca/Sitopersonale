b3fe8a14aabc614c4ff74434ced0d33b90d4601f
AntonioSusca <antonio.esse91@gmail.com>
2024-08-20 18:59:15 +0200

    primemodifiche

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,23 +2,24 @@
 
 
 
-<body class="bg-slate-200">
-    <h1 class="underline text-xl m-3 justify-center text-center "> Hello World </h1>
-    <div class="flex">
-        <div class="flex-1 rounded-xl bg-slate-500 p-10 m-2 ">01</div>
-        <div class="flex-1 rounded-xl bg-slate-500 p-10 m-2 ">02</div>
-    </div>
+    <body class="bg-slate-200 justify-center 	">
 
+        <div class="flex mt-8 h-screen ">
+            <div class="  flex-auto rounded-xl bg-slate-500 m-1 p-10  justify-center  h-4/5">Foto</div>
+            <div class="  flex-auto rounded-xl bg-slate-500 p-10 m-1  justify-center h-4/5">02</div>
 
-    <div class="rounded-xl bg-slate-500 p-2 m-4  ">
-        <div class=" flex-1 red bg-red-600 ">
-        </div>
-        <div class=" flex-1 bg-slate-700 ">
         </div>
 
-        <p class="text-center"> Ciao mi chiamo Antonio Susca</p>
+        <div class="rounded-xl bg-slate-500 p-2 m-4  ">
+            <div class=" flex-1 red bg-red-600 ">
+            </div>
+            <div class=" flex-1 bg-slate-700 ">
+            </div>
 
-    </div>
-    <h1 class="text-center text-black"><a href="/aboutme">ABOUT ME</a></h1>
+            <p class="text-center"> Ciao mi chiamo Antonio Susca</p>
+
+        </div>
+
+        <h1 class="text-center text-black"><a href="/aboutme">ABOUT ME</a></h1>
 
 </x-layout>
